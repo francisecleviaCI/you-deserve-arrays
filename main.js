0a66218fcd466aa927e2a721d667f0646838c7c7
Francis Eclevia <franciseclevia@Franciss-iMac.local>
2020-06-25 15:41:30 -0400

    update

--- main.js
+++ main.js
@@ -2,6 +2,58 @@
  * YOUR CODE HERE *
  ******************/
 
+//#1//
+function getFirstItemFrom(arr){
+  return arr[0]
+}
+console.log(getFirstItemFrom('francis, eclevia'))
+
+//#2//
+function getLastItemFrom(arr){
+  return arr[arr.length]
+}
+console.log(getLastItemFrom['francis, eclevia'])
+
+//#3//** */
+
+function getIndex3(arr){
+  return arr[3]
+}
+console.log(getIndex3('Hello, Mr, Francis'))
+
+//#4//
+function isLongList(arr){
+
+  if (arr.length <= 10){
+    return false; 
+  }
+}
+console.log(isLongList('francis'))
+
+//#5//*** */
+function firstItemIsNumber(arr){
+
+  if (arr[0] === num){
+    return true;
+  }
+}
+  console.log(isLongList(5))
+
+  //#6//
+  function secondCharOThirdString(arr){
+    return arr[2][1];
+
+  }
+  console.log(secondCharOThirdString('My Name Is Francis'))
+
+
+
+
+
+
+
+
+
 
 
 
